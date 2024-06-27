439fc520cfff550cffcd49770b3a15a80105a5d8
SuperDed34 <maxnozord@gmail.com>
2024-06-27 13:19:04 +0100

    refactor: optimize Header component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,42 +1,32 @@
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
-import IconButton from '@mui/material/IconButton'
-import InsertChartIcon from '@mui/icons-material/InsertChart'
-
-import { colors } from '../colors'
+import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import InsertChartIcon from '@mui/icons-material/InsertChart';
+import { colors } from '../colors';
 
 const Header = () => {
-
-
   return (
-    <Box>
-      <AppBar
-        position='static'
-        sx={{
-          backgroundColor: colors.componentBg,
-        }}>
-        <Toolbar>
-          <IconButton
-            size='large'
-            edge='start'
-            color='inherit'
-            aria-label='logo'
-            sx={{ mr: 2 }}>
-            <InsertChartIcon/>
-          </IconButton>
-          <Typography
-            variant='h6'
-            component={'div'}
-            sx={{ flexGrow: 1, cursor: 'default' }}>
-              Simple Tasks Manager
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
-    ) 
+    <AppBar
+      position='static'
+      sx={{ backgroundColor: colors.componentBg }}
+    >
+      <Toolbar>
+        <IconButton
+          size='large'
+          edge='start'
+          color='inherit'
+          aria-label='logo'
+          sx={{ mr: 2 }}
+        >
+          <InsertChartIcon />
+        </IconButton>
+        <Typography
+          variant='h6'
+          sx={{ flexGrow: 1, cursor: 'default' }}
+        >
+          Simple Tasks Manager
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
 
-export default Header
+export default Header;
